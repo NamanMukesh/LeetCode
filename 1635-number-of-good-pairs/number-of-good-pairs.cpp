ae2e2325b05582223921c2151b312f9946224b4b
NamanMukesh <147741741+NamanMukesh@users.noreply.github.com>
2024-03-30 10:57:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (70.15%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -4,8 +4,8 @@ public:
         int n=nums.size();
         int goodpairs=0;
         for (int i=0; i<n; i++){
-            for (int j=0; j<n; j++){
-                if (nums[i]==nums[j] && i<j){
+            for (int j=i+1; j<n; j++){
+                if (nums[i]==nums[j]){
                     goodpairs++;
                 }
             }
