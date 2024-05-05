70ff3120c93c592af04c6ded9203a400bed559bc
NamanMukesh <147741741+NamanMukesh@users.noreply.github.com>
2024-05-05 18:07:35 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (83.36%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
-        char findTheDifference(string s, string t) {
-        vector<int> mpp(26,0);
+    char findTheDifference(string s, string t) {
+        vector<int> mpp(26, 0);
         const int t1 = s.size();
         const int t2 = t.size();
-        for(int i=0;i<t2;++i){mpp[t[i]-'a']++;}
-        for(int i=0;i<t1;++i){mpp[s[i]-'a']--;}
-        for(int i=0;i<26;++i){if(mpp[i]==1){return i+'a';}}
+        for (int i = 0; i < t2; i++) {
+            mpp[t[i] - 'a']++;
+        }
+        for (int j = 0; j < t1; j++) {
+            mpp[s[j] - 'a']--;
+        }
+        for (int k = 0; k < 26; k++) {
+            if (mpp[k] == 1) {
+                return k + 'a';
+            }
+        }
         return 'a';
     }
 };
