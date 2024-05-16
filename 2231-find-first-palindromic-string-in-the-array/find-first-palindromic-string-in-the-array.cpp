d74b1e75ae25480c8adcbe74ed7d8e60ae5dd5ad
NamanMukesh <147741741+NamanMukesh@users.noreply.github.com>
2024-05-16 19:55:29 +0530

    Time: 72 ms (5.89%) | Memory: 33.1 MB (5.49%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -1,11 +1,28 @@
+// class Solution {
+// public:
+//     string firstPalindrome(vector<string>& words) {
+//       for (int i=0; i<words.size(); i++){
+//         string first=words[i];
+//         reverse(first.begin(), first.end());
+//         if (first==words[i]){
+//             return first;
+//         }  
+//       }
+//       return "";
+//     }
+// };
+
 class Solution {
 public:
     string firstPalindrome(vector<string>& words) {
       for (int i=0; i<words.size(); i++){
         string first=words[i];
-        reverse(first.begin(), first.end());
-        if (first==words[i]){
-            return first;
+        string rev="";
+        for (int j=first.size()-1; j>=0; j--){
+            rev+=first[j];
+            if (first==rev){
+                return first;
+            }
         }  
       }
       return "";
